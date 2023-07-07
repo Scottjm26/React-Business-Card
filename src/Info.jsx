1122c9e6eed8e7e3ac9b45b6709b8aa68a6d1b2b
scott <scottjm@nsuok.edu>
2023-07-07 13:56:05 -0500

    src

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,8 +1,9 @@
 import React from "react"
+import cat from "./cat.jpg"
  
  export default function Info (){
      return <div className ="info" >
-                <img className ="cat-image" src ="../images/cat.jpg"/>
+                <img className ="cat-image" src={cat} alt="cat" />;
                 <h1>Joshua Scott</h1>
                 <h2>Aspiring Frontend Developer</h2>
                 <p className ="website">Joshua.website</p>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+// import React from 'react'
+// import App from './App.jsx'
+// import './index.css'
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import Info from "./Info.jsx"
+import About from "./About.jsx"
+import Interest from "./Interest.jsx"
+import Footer from "./Footer.jsx"
+import Style from "./Style.css"
+
+
+function App(){
+    return <div className="body"><Info/>
+            <About/>
+            <Interest/>
+            <Footer/>
+            </div>
+}
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+  
